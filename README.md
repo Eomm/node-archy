c038adffbae2b2c2d896e03775d9dab069740a04
Manuel Spigolon <behemoth89@gmail.com>
2023-06-25 09:01:04 +0200

    washing

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 
 Render nested hierarchies `npm ls` style with unicode pipes.
 
-[![browser support](http://ci.testling.com/substack/node-archy.png)](http://ci.testling.com/substack/node-archy)
 
-[![build status](https://secure.travis-ci.org/substack/node-archy.png)](http://travis-ci.org/substack/node-archy)
 
 # example
 
 ``` js
-var archy = require('archy');
-var s = archy({
+const archy = require('@eomm/archy');
+const s = archy({
   label : 'beep',
   nodes : [
     'ity',
@@ -52,7 +50,7 @@ beep
 
 # methods
 
-var archy = require('archy')
+  const archy = require('@eomm/archy')
 
 ## archy(obj, prefix='', opts={})
 
--- index.js
+++ index.js
@@ -1,35 +1,35 @@
+'use strict'
+
 module.exports = function archy (obj, prefix, opts) {
-    if (prefix === undefined) prefix = '';
-    if (!opts) opts = {};
-    var chr = function (s) {
-        var chars = {
-            '│' : '|',
-            '└' : '`',
-            '├' : '+',
-            '─' : '-',
-            '┬' : '-'
-        };
-        return opts.unicode === false ? chars[s] : s;
-    };
-    
-    if (typeof obj === 'string') obj = { label : obj };
-    
-    var nodes = obj.nodes || [];
-    var lines = (obj.label || '').split('\n');
-    var splitter = '\n' + prefix + (nodes.length ? chr('│') : ' ') + ' ';
-    
-    return prefix
-        + lines.join(splitter) + '\n'
-        + nodes.map(function (node, ix) {
-            var last = ix === nodes.length - 1;
-            var more = node.nodes && node.nodes.length;
-            var prefix_ = prefix + (last ? ' ' : chr('│')) + ' ';
-            
-            return prefix
-                + (last ? chr('└') : chr('├')) + chr('─')
-                + (more ? chr('┬') : chr('─')) + ' '
-                + archy(node, prefix_, opts).slice(prefix.length + 2)
-            ;
+  if (prefix === undefined) prefix = ''
+  if (!opts) opts = {}
+  const chr = function (s) {
+    const chars = {
+      '│': '|',
+      '└': '`',
+      '├': '+',
+      '─': '-',
+      '┬': '-'
+    }
+    return opts.unicode === false ? chars[s] : s
+  }
+
+  if (typeof obj === 'string') obj = { label: obj }
+
+  const nodes = obj.nodes || []
+  const lines = (obj.label || '').split('\n')
+  const splitter = '\n' + prefix + (nodes.length ? chr('│') : ' ') + ' '
+
+  return prefix +
+    lines.join(splitter) + '\n' +
+    nodes.map(function (node, ix) {
+      const last = ix === nodes.length - 1
+      const more = node.nodes && node.nodes.length
+      const prefix_ = prefix + (last ? ' ' : chr('│')) + ' '
+
+          return prefix +
+            (last ? chr('└') : chr('├')) + chr('─') +
+            (more ? chr('┬') : chr('─')) + ' ' +
+            archy(node, prefix_, opts).slice(prefix.length + 2)
         }).join('')
-    ;
-};
+}
